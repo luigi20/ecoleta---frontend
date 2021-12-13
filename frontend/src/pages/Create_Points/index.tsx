3577db3902a4ea63ece74a2413873d675890a4a0
Luis <luisopentec@gmail.com>
2021-12-13 17:26:38 -0300

    building collection points page and api connection

--- frontend/src/pages/Create_Points/index.tsx
+++ frontend/src/pages/Create_Points/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
 import logo from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
+
+interface Item {
+    id: number;
+    name: string;
+    image_url: string;
+}
 const CreatePoint = () => {
+
+    const [items, setItems] = useState<Item[]>([]);
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data.serializedItems);
+        });
+    }, []);
     return (
         <div id="page-create-point">
             <header>
@@ -65,13 +80,13 @@ const CreatePoint = () => {
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado (UF)</label>
-                            <select name="uf" id="uf">
+                            <select id="uf" name="uf">
                                 <option value="0">Selecione uma UF</option>
                             </select>
                         </div>
                         <div className="field">
                             <label htmlFor="city">Cidade </label>
-                            <select name="city" id="city">
+                            <select id="city" name="city" >
                                 <option value="0">Selecione uma Cidade</option>
                             </select>
                         </div>
@@ -84,32 +99,16 @@ const CreatePoint = () => {
                         <span>Selecione um ou mais itens abaixo</span>
                     </legend>
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt={item.name} />
+                                <span>{item.name}</span>
+                            </li>
+                        ))}
+
                     </ul>
                 </fieldset>
+
                 <button type="submit" >Cadastrar Ponto de Coleta</button>
             </form>
         </div>
